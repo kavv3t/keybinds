b6812ff9a1955834365ae9a9ba58f6101853e11e
kavv3t <153438488+kavv3t@users.noreply.github.com>
2025-05-15 17:24:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ addEventListener("keydown", (event) => {
     document.querySelector('.code').innerHTML = event.code;
     console.log(event);
 });
+
+document.addEventListenrer("mousedown", (e) => {
+    e.preventDefault();
+    document.querySelector('.text').innerHTML = e.key;
+    document.querySelector('.code').innerHTML = e.code;
+    console.log(event);
+})
